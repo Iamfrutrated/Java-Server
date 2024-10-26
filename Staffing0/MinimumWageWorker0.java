8272980504acbb03bca15743ffbc69f747ee9868
Java Server <133314631+Iamfrutrated@users.noreply.github.com>
2024-10-25 20:48:15 -0700

    Update MinimumWageWorker0.java

--- Staffing0/MinimumWageWorker0.java
+++ Staffing0/MinimumWageWorker0.java
@@ -1,9 +1,12 @@
-public class MinimumWageWorker0 extends Staff0
+public class MinimumWageWorker0 extends Staff0 	// MinimumWageWorker0 extends Staff0, allowing it access to the protected field variables
+						// income and money. It also grants access the work() method, however that is not
+						// demonstrated here.
 {
-	public MinimumWageWorker0()
+	public MinimumWageWorker0() // no-args constructor
 	{
-		income = 1;
-		money = 1;
+		
+		income = 1; 	// The income and money variables were never declared yet they can be used because they were derived from
+		money = 1; 	// the parent class, Staff0, which had all of these variables. Note that these two lines do not directly
+				// affect the field variables in the parent class, they only change the value in this particular class
 	}
-			
 }
