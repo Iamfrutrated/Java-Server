83f6e8cd9c2d07a2de636a64f90860cd1e0c4228
Java Server <133314631+Iamfrutrated@users.noreply.github.com>
2024-10-25 20:36:35 -0700

    Add files via upload

---
+++ Staffing0/Staff0.java
@@ -0,0 +1,15 @@
+public class Staff0
+{
+	protected float income;
+	protected float money;
+	
+	public Staff0()
+	{
+		// no-args constructor
+	}
+	
+	public void work()
+	{
+		System.out.println("You worked for no pay.");
+	} 
+}
