039db1766d016de81defe7412ee783755747593b
Java Server <133314631+Iamfrutrated@users.noreply.github.com>
2024-10-28 20:57:40 -0700

    Update Science0.java

--- Inheritance/Subjects0/Science0.java
+++ Inheritance/Subjects0/Science0.java
@@ -1,19 +1,24 @@
-public class Science0 extends Subjects0
+public class Science0 extends Subjects0	// Science can use all the methods and all the field variables(protected) from Subjects0
 {
-	protected String fieldName;
-	protected boolean needsCriticalThinking;
+	protected String fieldName;	// These variables can be passed on to subclasses (Biology0)
+	protected boolean needsCriticalThinking;	
 	protected int newVocabScale; // 1 - 100 
 	
-	public Science0(String fieldNameIn, boolean nCTIn, int newVocabScaleIn)
+	public Science0(String fieldNameIn, boolean nCTIn, int newVocabScaleIn)	// constructor for Science0 with values
+										// because Biology0 passes values into this constructor
+										// the values passed in from Biology0 will directly
+										// affect the values here.
 	{
-		fieldName = fieldNameIn;
+		// These simply assign values passed in from Biology0 into this constructor to the field variables
+		
+		fieldName = fieldNameIn;	
 		needsCriticalThinking = nCTIn;
 		newVocabScale = newVocabScaleIn;
 	}
 	
-	public void printAction()
+	public void printAction()	// print method
 	{
-		if(needsCriticalThinking) // not officially taught yet
+		if(needsCriticalThinking)
 		{
 			System.out.printf("You studied %s which requires critical thinking, needs an intelligence of %d, and has rating of %d for new vocab %n", 
 								fieldName, intelligenceReq, newVocabScale);
