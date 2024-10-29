e8752f211c348a4d0f525559913fc01abc051715
Java Server <133314631+Iamfrutrated@users.noreply.github.com>
2024-10-28 20:52:04 -0700

    Update Biology0.java

--- Inheritance/Subjects0/Biology0.java
+++ Inheritance/Subjects0/Biology0.java
@@ -1,8 +1,9 @@
-public class Biology0 extends Science0
+public class Biology0 extends Science0	// Biology0 possesses all the methods and field variables(that are protected) that Science0 gets
 {
-	public Biology0(String fieldNameIn, boolean nCTIn, int newVocabScaleIn)
+	public Biology0(String fieldNameIn, boolean nCTIn, int newVocabScaleIn) // constructor that takes in values and passes them on to 
+										// the super class
 	{
-		super(fieldNameIn, nCTIn, newVocabScaleIn);
+		super(fieldNameIn, nCTIn, newVocabScaleIn);	// these values are being passed into Science0's constructor
 	}
 	
 }
